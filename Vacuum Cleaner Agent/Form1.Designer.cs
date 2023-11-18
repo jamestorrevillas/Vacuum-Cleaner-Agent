cdb086c260adfbdd9798494c5ff42b476167468d
TorrevillasJames <james.torrevillas@cit.edu>
2023-11-18 22:12:14 +0800

    Add project files.

---
+++ Vacuum Cleaner Agent/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Vacuum_agent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_Room_B = new System.Windows.Forms.Label();
+            this.label_Room_A = new System.Windows.Forms.Label();
+            this.btn_Clean = new System.Windows.Forms.Button();
+            this.btn_NoOperation = new System.Windows.Forms.Button();
+            this.label_RoomState_A = new System.Windows.Forms.Label();
+            this.label_RoomState_B = new System.Windows.Forms.Label();
+            this.label_RoomState_D = new System.Windows.Forms.Label();
+            this.label_RoomState_C = new System.Windows.Forms.Label();
+            this.label_Room_C = new System.Windows.Forms.Label();
+            this.label_Room_D = new System.Windows.Forms.Label();
+            this.textBox_ProcessInformation = new System.Windows.Forms.TextBox();
+            this.btn_ResetToInitialState = new System.Windows.Forms.Button();
+            this.pictureBox_Room_C = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Room_D = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Room_A = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Room_B = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Room_C)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Room_D)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Room_A)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Room_B)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_Room_B
+            // 
+            this.label_Room_B.AutoSize = true;
+            this.label_Room_B.Location = new System.Drawing.Point(379, 254);
+            this.label_Room_B.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Room_B.Name = "label_Room_B";
+            this.label_Room_B.Size = new System.Drawing.Size(63, 16);
+            this.label_Room_B.TabIndex = 1;
+            this.label_Room_B.Text = "Room B -";
+            // 
+            // label_Room_A
+            // 
+            this.label_Room_A.AutoSize = true;
+            this.label_Room_A.Location = new System.Drawing.Point(94, 254);
+            this.label_Room_A.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Room_A.Name = "label_Room_A";
+            this.label_Room_A.Size = new System.Drawing.Size(63, 16);
+            this.label_Room_A.TabIndex = 1;
+            this.label_Room_A.Text = "Room A -";
+            // 
+            // btn_Clean
+            // 
+            this.btn_Clean.Location = new System.Drawing.Point(694, 326);
+            this.btn_Clean.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Clean.Name = "btn_Clean";
+            this.btn_Clean.Size = new System.Drawing.Size(152, 50);
+            this.btn_Clean.TabIndex = 3;
+            this.btn_Clean.Text = "Clean";
+            this.btn_Clean.UseVisualStyleBackColor = true;
+            this.btn_Clean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // btn_NoOperation
+            // 
+            this.btn_NoOperation.Location = new System.Drawing.Point(694, 395);
+            this.btn_NoOperation.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_NoOperation.Name = "btn_NoOperation";
+            this.btn_NoOperation.Size = new System.Drawing.Size(152, 50);
+            this.btn_NoOperation.TabIndex = 3;
+            this.btn_NoOperation.Text = "No Operation";
+            this.btn_NoOperation.UseVisualStyleBackColor = true;
+            this.btn_NoOperation.Click += new System.EventHandler(this.btnNoOperation_Click);
+            // 
+            // label_RoomState_A
+            // 
+            this.label_RoomState_A.AutoSize = true;
+            this.label_RoomState_A.ForeColor = System.Drawing.Color.Red;
+            this.label_RoomState_A.Location = new System.Drawing.Point(158, 254);
+            this.label_RoomState_A.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_RoomState_A.Name = "label_RoomState_A";
+            this.label_RoomState_A.Size = new System.Drawing.Size(34, 16);
+            this.label_RoomState_A.TabIndex = 6;
+            this.label_RoomState_A.Text = "Dirty";
+            // 
+            // label_RoomState_B
+            // 
+            this.label_RoomState_B.AutoSize = true;
+            this.label_RoomState_B.ForeColor = System.Drawing.Color.Red;
+            this.label_RoomState_B.Location = new System.Drawing.Point(443, 254);
+            this.label_RoomState_B.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_RoomState_B.Name = "label_RoomState_B";
+            this.label_RoomState_B.Size = new System.Drawing.Size(34, 16);
+            this.label_RoomState_B.TabIndex = 6;
+            this.label_RoomState_B.Text = "Dirty";
+            // 
+            // label_RoomState_D
+            // 
+            this.label_RoomState_D.AutoSize = true;
+            this.label_RoomState_D.ForeColor = System.Drawing.Color.Red;
+            this.label_RoomState_D.Location = new System.Drawing.Point(443, 517);
+            this.label_RoomState_D.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_RoomState_D.Name = "label_RoomState_D";
+            this.label_RoomState_D.Size = new System.Drawing.Size(34, 16);
+            this.label_RoomState_D.TabIndex = 11;
+            this.label_RoomState_D.Text = "Dirty";
+            this.label_RoomState_D.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label_RoomState_C
+            // 
+            this.label_RoomState_C.AutoSize = true;
+            this.label_RoomState_C.ForeColor = System.Drawing.Color.Red;
+            this.label_RoomState_C.Location = new System.Drawing.Point(158, 517);
+            this.label_RoomState_C.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_RoomState_C.Name = "label_RoomState_C";
+            this.label_RoomState_C.Size = new System.Drawing.Size(34, 16);
+            this.label_RoomState_C.TabIndex = 12;
+            this.label_RoomState_C.Text = "Dirty";
+            this.label_RoomState_C.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label_Room_C
+            // 
+            this.label_Room_C.AutoSize = true;
+            this.label_Room_C.Location = new System.Drawing.Point(94, 517);
+            this.label_Room_C.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Room_C.Name = "label_Room_C";
+            this.label_Room_C.Size = new System.Drawing.Size(63, 16);
+            this.label_Room_C.TabIndex = 9;
+            this.label_Room_C.Text = "Room C -";
+            this.label_Room_C.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label_Room_D
+            // 
+            this.label_Room_D.AutoSize = true;
+            this.label_Room_D.Location = new System.Drawing.Point(379, 516);
+            this.label_Room_D.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_Room_D.Name = "label_Room_D";
+            this.label_Room_D.Size = new System.Drawing.Size(64, 16);
+            this.label_Room_D.TabIndex = 10;
+            this.label_Room_D.Text = "Room D -";
+            this.label_Room_D.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox_ProcessInformation
+            // 
+            this.textBox_ProcessInformation.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox_ProcessInformation.Location = new System.Drawing.Point(600, 32);
+            this.textBox_ProcessInformation.Multiline = true;
+            this.textBox_ProcessInformation.Name = "textBox_ProcessInformation";
+            this.textBox_ProcessInformation.ReadOnly = true;
+            this.textBox_ProcessInformation.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_ProcessInformation.Size = new System.Drawing.Size(342, 265);
+            this.textBox_ProcessInformation.TabIndex = 2;
+            // 
+            // btn_ResetToInitialState
+            // 
+            this.btn_ResetToInitialState.Location = new System.Drawing.Point(694, 462);
+            this.btn_ResetToInitialState.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_ResetToInitialState.Name = "btn_ResetToInitialState";
+            this.btn_ResetToInitialState.Size = new System.Drawing.Size(152, 50);
+            this.btn_ResetToInitialState.TabIndex = 13;
+            this.btn_ResetToInitialState.Text = "Reset to Initial State";
+            this.btn_ResetToInitialState.UseVisualStyleBackColor = true;
+            this.btn_ResetToInitialState.Click += new System.EventHandler(this.btnResetToInitialState_Click);
+            // 
+            // pictureBox_Room_C
+            // 
+            this.pictureBox_Room_C.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Room_C.Image = global::Vaccum_agent.Properties.Resources.v1_r;
+            this.pictureBox_Room_C.Location = new System.Drawing.Point(26, 295);
+            this.pictureBox_Room_C.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox_Room_C.Name = "pictureBox_Room_C";
+            this.pictureBox_Room_C.Size = new System.Drawing.Size(247, 217);
+            this.pictureBox_Room_C.TabIndex = 7;
+            this.pictureBox_Room_C.TabStop = false;
+            this.pictureBox_Room_C.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox_Room_D
+            // 
+            this.pictureBox_Room_D.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Room_D.Image = global::Vaccum_agent.Properties.Resources.v1_l;
+            this.pictureBox_Room_D.Location = new System.Drawing.Point(309, 295);
+            this.pictureBox_Room_D.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox_Room_D.Name = "pictureBox_Room_D";
+            this.pictureBox_Room_D.Size = new System.Drawing.Size(247, 217);
+            this.pictureBox_Room_D.TabIndex = 8;
+            this.pictureBox_Room_D.TabStop = false;
+            this.pictureBox_Room_D.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox_Room_A
+            // 
+            this.pictureBox_Room_A.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Room_A.Image = global::Vaccum_agent.Properties.Resources.v1_r;
+            this.pictureBox_Room_A.Location = new System.Drawing.Point(26, 32);
+            this.pictureBox_Room_A.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox_Room_A.Name = "pictureBox_Room_A";
+            this.pictureBox_Room_A.Size = new System.Drawing.Size(247, 217);
+            this.pictureBox_Room_A.TabIndex = 0;
+            this.pictureBox_Room_A.TabStop = false;
+            // 
+            // pictureBox_Room_B
+            // 
+            this.pictureBox_Room_B.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_Room_B.Location = new System.Drawing.Point(309, 32);
+            this.pictureBox_Room_B.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox_Room_B.Name = "pictureBox_Room_B";
+            this.pictureBox_Room_B.Size = new System.Drawing.Size(247, 217);
+            this.pictureBox_Room_B.TabIndex = 0;
+            this.pictureBox_Room_B.TabStop = false;
+            this.pictureBox_Room_B.Click += new System.EventHandler(this.pictureBox_Room_B_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(982, 603);
+            this.Controls.Add(this.btn_ResetToInitialState);
+            this.Controls.Add(this.textBox_ProcessInformation);
+            this.Controls.Add(this.label_RoomState_D);
+            this.Controls.Add(this.label_RoomState_C);
+            this.Controls.Add(this.label_Room_C);
+            this.Controls.Add(this.label_Room_D);
+            this.Controls.Add(this.pictureBox_Room_C);
+            this.Controls.Add(this.pictureBox_Room_D);
+            this.Controls.Add(this.label_RoomState_B);
+            this.Controls.Add(this.label_RoomState_A);
+            this.Controls.Add(this.btn_NoOperation);
+            this.Controls.Add(this.btn_Clean);
+            this.Controls.Add(this.label_Room_A);
+            this.Controls.Add(this.label_Room_B);
+            this.Controls.Add(this.pictureBox_Room_A);
+            this.Controls.Add(this.pictureBox_Room_B);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximumSize = new System.Drawing.Size(1000, 800);
+            this.MinimumSize = new System.Drawing.Size(866, 592);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Room_C)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Room_D)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Room_A)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Room_B)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_Room_A;
+        private System.Windows.Forms.PictureBox pictureBox_Room_B;
+        private System.Windows.Forms.PictureBox pictureBox_Room_C;
+        private System.Windows.Forms.PictureBox pictureBox_Room_D;
+        private System.Windows.Forms.Label label_Room_A;
+        private System.Windows.Forms.Label label_Room_B;
+        private System.Windows.Forms.Label label_Room_C;
+        private System.Windows.Forms.Label label_Room_D;
+        private System.Windows.Forms.Label label_RoomState_A;
+        private System.Windows.Forms.Label label_RoomState_B;
+        private System.Windows.Forms.Label label_RoomState_D;
+        private System.Windows.Forms.Label label_RoomState_C;
+        private System.Windows.Forms.Button btn_Clean;
+        private System.Windows.Forms.Button btn_NoOperation;
+        private System.Windows.Forms.Button btn_ResetToInitialState;
+        private System.Windows.Forms.TextBox textBox_ProcessInformation;
+
+    }
+}
+
---
+++ Vacuum Cleaner Agent/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vacuum_agent
+{
+
+    public partial class Form1 : Form
+    {
+        string[] rooms = new string[] { "A", "B", "C", "D" };
+        string[] rooms_status = new string[] { "Dirty", "Dirty", "Dirty", "Dirty" };
+        int Current_Room = 0;
+        Dictionary<string, Label> status_label = new Dictionary<string, Label>();
+        Dictionary<string, PictureBox> imagebox = new Dictionary<string, PictureBox>();
+        Dictionary<string, Image> imagebox_normal = new Dictionary<string, Image>();
+        Dictionary<string, Image> imagebox_suck = new Dictionary<string, Image>();
+        bool end_loop = false;
+        private bool isProcessing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox_Room_A.Image = null;
+            pictureBox_Room_B.Image = null;
+            pictureBox_Room_B.Image = null;
+            pictureBox_Room_C.Image = null;
+            pictureBox_Room_D.Image = null;
+
+            textBox_ProcessInformation.Text = "Initial state: All Rooms are dirty. Randomize start state.\r\nGoal: All rooms are clean.\r\n";
+            textBox_ProcessInformation.ForeColor = Color.Black;
+            textBox_ProcessInformation.Select(0, 0);
+
+            status_label.Add("A", label_RoomState_A);
+            status_label.Add("B", label_RoomState_B);
+            status_label.Add("C", label_RoomState_C);
+            status_label.Add("D", label_RoomState_D);
+
+            imagebox.Add("A", pictureBox_Room_A);
+            imagebox.Add("B", pictureBox_Room_B);
+            imagebox.Add("C", pictureBox_Room_C);
+            imagebox.Add("D", pictureBox_Room_D);
+
+            imagebox_normal.Add("A", Vaccum_agent.Properties.Resources.v1_r);
+            imagebox_normal.Add("B", Vaccum_agent.Properties.Resources.v1_l);
+            imagebox_normal.Add("C", Vaccum_agent.Properties.Resources.v1_r);
+            imagebox_normal.Add("D", Vaccum_agent.Properties.Resources.v1_l);
+
+            imagebox_suck.Add("A", Vaccum_agent.Properties.Resources.v1_rs);
+            imagebox_suck.Add("B", Vaccum_agent.Properties.Resources.v1_ls);
+            imagebox_suck.Add("C", Vaccum_agent.Properties.Resources.v1_rs);
+            imagebox_suck.Add("D", Vaccum_agent.Properties.Resources.v1_ls);
+        }
+
+        private void AgentOperation(object sender, DoWorkEventArgs e)
+        {
+            this.Invoke(new MethodInvoker(delegate
+            {
+                textBox_ProcessInformation.AppendText("Agent started!\r\n" +
+                    "Current Room " + rooms[Current_Room] + "\r\n");
+            }));
+            System.Threading.Thread.Sleep(1000);
+
+            while (isProcessing)
+            {
+                end_loop = false;
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    textBox_ProcessInformation.AppendText("Percept!\r\n");
+                }));
+                System.Threading.Thread.Sleep(1000);
+
+                if (rooms_status[Current_Room] == "Dirty")
+                {
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        textBox_ProcessInformation.AppendText("Dirt found.\r\n");
+                    }));
+                    System.Threading.Thread.Sleep(1000);
+
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        Suck();
+                    }));
+                }
+                else
+                {
+                    this.Invoke(new MethodInvoker(delegate
+                    {
+                        textBox_ProcessInformation.AppendText("Room is clean.\r\n");
+                    }));
+                }
+                System.Threading.Thread.Sleep(1000);
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    MoveAgent();
+                }));
+                System.Threading.Thread.Sleep(1000);
+                end_loop = true;
+            }
+        }
+
+        void Suck()
+        {
+            if (isProcessing)
+            {
+                imagebox[rooms[Current_Room]].Image = imagebox_suck[rooms[Current_Room]];
+                textBox_ProcessInformation.AppendText("Suck!\r\n");
+                rooms_status[Current_Room] = "Clean";
+                status_label[rooms[Current_Room]].Text = rooms_status[Current_Room];
+                status_label[rooms[Current_Room]].ForeColor = Color.Green;
+            }
+        }
+
+        void MoveAgent()
+        {
+            if (isProcessing)
+            {
+                imagebox[rooms[Current_Room]].Image = null;
+                int nextRoom = FindNextRoom();
+                Current_Room = nextRoom;
+                textBox_ProcessInformation.AppendText("Move to room " + rooms[Current_Room] + "\r\n");
+                imagebox[rooms[Current_Room]].Image = imagebox_normal[rooms[Current_Room]];
+            }
+        }
+
+        int FindNextRoom()
+        {
+            Random random = new Random();
+            int nextRoom = random.Next(rooms.Length);
+            if (isProcessing)
+            {
+                do
+                {
+                    nextRoom = random.Next(rooms.Length);
+                } while (nextRoom == Current_Room);
+            }
+
+            return nextRoom;
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            if (!isProcessing)
+            {
+                isProcessing = true;
+                Random random = new Random();
+                Current_Room = random.Next(rooms.Length);
+
+                imagebox[rooms[Current_Room]].Image = imagebox_normal[rooms[Current_Room]];
+
+                BackgroundWorker bw = new BackgroundWorker();
+                bw.DoWork += new DoWorkEventHandler(AgentOperation);
+
+                if (bw.IsBusy != true)
+                {
+                    bw.RunWorkerAsync();
+                }
+            }
+        }
+
+        private void btnNoOperation_Click(object sender, EventArgs e)
+        {
+            isProcessing = false;
+            imagebox[rooms[Current_Room]].Image = null;
+            textBox_ProcessInformation.AppendText("Operation stopped. " + "\r\n");
+        }
+
+        private void btnResetToInitialState_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < rooms_status.Length; i++)
+            {
+                rooms_status[i] = "Dirty";
+                status_label[rooms[i]].Text = rooms_status[i];
+                status_label[rooms[i]].ForeColor = Color.Red;
+            }
+            textBox_ProcessInformation.AppendText("Room states reset to dirty. " + "\r\n");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox_Room_B_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Variable
+    {
+        public object Value { get; set; }
+    }
+}
